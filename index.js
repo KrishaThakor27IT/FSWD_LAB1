f10235bde4120e8b329cd86fdbb76cd9658b94a1
KrishaThakor27IT <23it132@charusat.edu.in>
2024-12-17 15:36:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const movies = [
+    { title: "Pushpa 2", genre: "Action Thriller", rating: 8.8, releaseYear: 2010 },
+    { title: "K.G.F Chapter 2", genre: "Action", rating: 9.0, releaseYear: 2008 },
+    { title: "Housefull 4", genre: "Comedy", rating: 8.6, releaseYear: 2014 }
+];
+const addMovie = (collection, movie) => {
+    collection.push(movie);
+};
+
+addMovie(movies, { title: "Genius", genre: "Action", rating: 7.5, releaseYear: 2020 });
+
+const listMoviesByGenre = (collection, genre) => {
+    return collection.filter(movie => movie.genre === genre);
+};
+
+console.log(listMoviesByGenre(movies, "Sci-Fi"));
+const findHighestRatedMovie = collection => {
+    return collection.reduce((highest, movie) => movie.rating > highest.rating ? movie : highest);
+};
+
+console.log(findHighestRatedMovie(movies));
+const getMovieTitles = collection => {
+    return collection.map(movie => movie.title);
+};
+
+console.log(getMovieTitles(movies));
+const moviesAfterYear = (collection, year) => {
+    return collection.filter(movie => movie.releaseYear > year);
+};
+
+console.log(moviesAfterYear(movies, 2010));
+movies.forEach(movie => {
+    console.log(`${movie.title} (${movie.releaseYear}) is a ${movie.genre} movie with a rating of ${movie.rating}.`);
+});
+
